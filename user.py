af1c7f2bc74c3387d5cd2a5c99b514bc517cf6cf
NeryCaballero <nerycaballero24@gmail.com>
2021-03-14 23:03:48 +0100

    Created User model

---
+++ user.py
@@ -0,0 +1,7 @@
+class User():
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
+
+# id is a python keyword. We don't want to use it as a variable.
